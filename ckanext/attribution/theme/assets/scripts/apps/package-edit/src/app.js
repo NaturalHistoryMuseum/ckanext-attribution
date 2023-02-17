e559884189bc8825bb88f294c856a32bb6db2269
Alice Butcher <alycejenni@gmail.com>
2023-02-17 11:25:27 +0000

    style: reformat with prettier

--- ckanext/attribution/theme/assets/scripts/apps/package-edit/src/app.js
+++ ckanext/attribution/theme/assets/scripts/apps/package-edit/src/app.js
@@ -8,18 +8,18 @@ import SelectField from './components/fields/SelectField.vue';
 import Autocomplete from './components/fields/Autocomplete.vue';
 import AutocompleteList from './components/fields/AutocompleteList.vue';
 
-Vue.component('app', App)
+Vue.component('app', App);
 
 // fields
-Vue.component('help-tooltip', Help)
-Vue.component('input-field', Field)
-Vue.component('text-field', TextField)
-Vue.component('select-field', SelectField)
-Vue.component('autocomplete-field', Autocomplete)
-Vue.component('autocomplete-list', AutocompleteList)
+Vue.component('help-tooltip', Help);
+Vue.component('input-field', Field);
+Vue.component('text-field', TextField);
+Vue.component('select-field', SelectField);
+Vue.component('autocomplete-field', Autocomplete);
+Vue.component('autocomplete-list', AutocompleteList);
 
 // app
 new Vue({
-    el: '#contributors',
-    store
+  el: '#contributors',
+  store,
 });
